74c621b7a0bf7d04755ff344476d9634141fb938
AnaDragan <33637539+AnaDragan@users.noreply.github.com>
2017-12-21 11:59:20 +0200

    Classes
    Classes basic implementation.

---
+++ ContainableItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class ContainableItem : Product, AuditableItem<ContainableItem>
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int Size { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
